44b51c9d1eb3ffb1f5dd5abf60e60d190619a0d8
aalyman42 <aalyman42@gmail.com>
2024-01-16 14:05:49 -0700

    added bcrypt function

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
-class Users extends Model {}
+class Users extends Model {
+  checkPassword(login) {
+    return bcrypt.compareSync(login, this.password);
+  }
+}
 
 Users.init(
   {
@@ -45,6 +50,21 @@ Users.init(
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.email = await newUserData.email.toLowerCase();
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.email = await updatedUserData.email.toLowerCase();
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
     modelName: "users",
   }
