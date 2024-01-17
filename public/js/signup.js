e135e7200639cd809105a920d15dac7f1e73c669
Chance Creger <chance.creger@gmail.com>
2024-01-17 13:57:23 -0700

    added login and sign up logic to js files

--- public/js/signup.js
+++ public/js/signup.js
@@ -14,11 +14,12 @@ document.querySelectorAll(".toggle-password").forEach(function (toggle) {
 
 var formData = {};
 
-$("#loginForm").on("submit", function (event) {
+$(".signin-Form").on("submit", function (event) {
   event.preventDefault();
 
   formData.username = document.getElementById("username-field").value;
   formData.email = document.getElementById("email-field").value;
+  formData.password = document.getElementById("password-field");
 
   $.ajax("https://api.emailjs.com/api/v1.0/email/send-form", {
     type: "POST",
@@ -34,3 +35,28 @@ $("#loginForm").on("submit", function (event) {
     });
 });
 //POST https://api.emailjs.com/api/v1.0/email/send-form
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector("#username-field").value.trim();
+  const email = document.querySelector("#email-field").value.trim();
+  const password = document.querySelector("#password-field").value.trim();
+
+  if (name && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupForm = document.querySelector(".signin-form");
+
+signupForm.addEventListener("submit", signupFormHandler);
